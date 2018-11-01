9611b4ecde94d25e1fb46b86d7bb688226a67b66
Mijian Xu <mijian.xu@smail.nju.edu.cn>
2018-11-01 11:51:57 +0800

    ccp_center_bin

--- Scripts/ccp_center_bin.py
+++ Scripts/ccp_center_bin.py
@@ -1,5 +1,11 @@
 import numpy as np
 from seispy.geo import distaz, km2deg
+from seispy.bootstrap import ci
+from scipy.io import savemat, loadmat
+import warnings
+
+
+warnings.filterwarnings("ignore")
 
 
 def initgrid(lat1, lon1, lat2, lon2, val):
@@ -30,13 +36,43 @@ def search_pierce(rfdep, depaxis, bin_loca, bin_radius=100):
     return data
 
 
+def boot_stack(ccp_data, bin_loca, depaxis):
+    stack_data = []
+    for i in range(ccp_data.shape[0]):
+        print('{}/{}'.format(i+1, bin_loca.shape[0]))
+        boot_stack = {}
+        bin_mu = np.zeros(depaxis.shape[0])
+        bin_ci = np.zeros([depaxis.shape[0], 2])
+        bin_count = np.zeros(depaxis.shape[0])
+        for j in range(depaxis.shape[0]):
+            bin_count[j] = ccp_data[i, j].shape[1]
+            if ccp_data[i, j].shape[1] > 1:
+                cci = ci(ccp_data[i, j], n_samples=2000)
+                bin_ci[j, 0] = cci[0]
+                bin_ci[j, 1] = cci[1]
+                bin_mu[j] = np.average(ccp_data[i, j])
+            else:
+                bin_ci[j, 0] = np.nan
+                bin_ci[j, 1] = np.nan
+                bin_mu[j] = np.nan
+        boot_stack['bin_lat'] = bin_loca[i, 0]
+        boot_stack['bin_lon'] = bin_loca[i, 1]
+        boot_stack['mu'] = bin_mu
+        boot_stack['ci'] = bin_ci
+        boot_stack['count'] = bin_count
+        stack_data.append(boot_stack)
+    return stack_data
+
+
 if __name__ == '__main__':
     lat1 = 25
     lon1 = 81
     lat2 = 39
     lon2 = 101
     bin_loca = initgrid(lat1, lon1, lat2, lon2, 0.5)
-    rfdep = np.load('/Volumes/xumj3/TibetRF/RFdepth_1D.npy')
+    # rfdep = np.load('/Volumes/xumj3/TibetRF/RFdepth_1D.npy')
     depaxis = np.arange(300, 800)
-    data = search_pierce(rfdep, depaxis, bin_loca)
-    np.savez('/Volumes/xumj3/TibetRF/ccp_data', ccp_data=data)
+    # data = search_pierce(rfdep, depaxis, bin_loca)
+    ccp_data = loadmat('/Users/xumj/Researches/Tibet_MTZ/ccp_data.mat')['ccp_data']
+    stack_data = boot_stack(ccp_data, bin_loca, depaxis)
+    np.save('/Users/xumj/Researches/Tibet_MTZ/ccp_results/stack_data', stack_data)
