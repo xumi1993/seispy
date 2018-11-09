458f3074b74de1c9e02fd975fb45b11d85c7d9bf
Mijian Xu <mijian.xu@smail.nju.edu.cn>
2018-11-09 18:57:59 +0800

    update ccp_profile

--- Scripts/ccp_profile.py
+++ Scripts/ccp_profile.py
@@ -2,6 +2,10 @@ import numpy as np
 from seispy.geo import distaz, km2deg, deg2km, latlon_from
 from seispy.bootstrap import ci
 from seispy.distaz import distaz
+from scipy.io import loadmat
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def init_profile(lat1, lon1, lat2, lon2, val):
@@ -21,16 +25,16 @@ def search_pierce(rfdep, depaxis, bin_loca, profile_range, bin_radius=75):
     data = []
     for i in range(bin_loca.shape[0]):
         rfbin = {}
-        print('{}/{}'.format(i, bin_loca.shape[0]))
+        print('{}/{}'.format(i+1, bin_loca.shape[0]))
         ccp_mean = np.zeros(depaxis.shape[0])
         ccp_count = np.zeros(depaxis.shape[0])
         ccp_ci = np.zeros((depaxis.shape[0], 2))
         for j, dep in zip(range(depaxis.shape[0]), depaxis):
             bin_dep = np.array([])
             for sta in rfdep:
-                fall_idx = np.where(distaz(sta['Piercelat'][dep, :], sta['Piercelon'][dep, :], bin_loca[i, 0],
-                                           bin_loca[i, 1]).delta < bin_radius)[0]
-                bin_dep = np.append(bin_dep, sta['moveout_correct'][dep, fall_idx])
+                fall_idx = np.where(distaz(sta['Piercelat'][0, 0][:, dep], sta['Piercelon'][0, 0][:, dep],
+                                           bin_loca[i, 0], bin_loca[i, 1]).delta < bin_radius)[0]
+                bin_dep = np.append(bin_dep, sta['moveout_correct'][0, 0][fall_idx, dep])
             if bin_dep.shape[0] > 1:
                 bin_ci = ci(bin_dep, n_samples=2000)
                 bin_mu = np.average(bin_dep)
@@ -52,12 +56,12 @@ def search_pierce(rfdep, depaxis, bin_loca, profile_range, bin_radius=75):
 
 
 if __name__ == '__main__':
-    lat1 = 37.8
-    lon1 = 96
-    lat2 = 25.8
-    lon2 = 92
+    lat1 = 35.8
+    lon1 = 85.3
+    lat2 = 25.5
+    lon2 = 88
     bin_loca, profile_range = init_profile(lat1, lon1, lat2, lon2, 25)
-    rfdep = np.load('/Users/xumj/Researches/Tibet_MTZ/RFdepth_1D.npy')
+    rfdep = loadmat('/Users/xumj/Researches/Tibet_MTZ/ccp_results/RFdepth_3D.mat')['RFdepth'][0, :]
     depaxis = np.arange(300, 800)
     ccp_data = search_pierce(rfdep, depaxis, bin_loca, profile_range)
-    np.save('/Users/xumj/Researches/Tibet_MTZ/ccp_B', ccp_data)
+    np.save('/Users/xumj/Researches/Tibet_MTZ/ccp_results/ccp_a_3D', ccp_data)
