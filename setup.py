e564446e66ab96e26bca1e557db7cd653f907991
Mijian Xu <gomijianxu@gmail.com>
2021-08-13 17:58:17 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ packages = find_packages()
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = "1.2.5"
+VERSION = "1.2.6"
 setup(name='python-seispy',
       version=VERSION,
       author='Mijian Xu',
