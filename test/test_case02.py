58e765d1b3213fb95f1468e671e06887f087f6ff
xumi1993 <gomijianxu@gmail.com>
2022-01-15 22:18:11 +0800

    Move test here

---
+++ test/test_case02.py
@@ -0,0 +1,18 @@
+from seispy.rfcorrect import RFStation
+import pytest
+from os.path import exists
+from subprocess import Popen
+
+
+def test_download():
+    if exists('ex-rfani.tar.gz'):
+        pytest.skip('Data are downloaded.')
+    s = 'wget https://osf.io/4hk6d/download -O ex-rfani.tar.gz\n'
+    s += 'tar -xzf ex-rfani.tar.gz\n'
+    proc = Popen(s, shell=True)
+    proc.communicate()
+
+
+def test_sub01():
+    rfs = RFStation('ex-rfani/SC.LTA')
+    rfs.jointani(3, 8, tlen=3.5, stack_baz_val=10, weight=[0.4, 0.4, 0.2])
---
+++ test/test_case03.py
@@ -0,0 +1,46 @@
+from seispy.ccppara import ccppara
+from seispy.ccpprofile import CCPProfile
+from seispy.rf2depth_makedata import makedata
+from os.path import exists
+from subprocess import Popen
+import pytest
+import os
+
+def test_download():
+    if exists('ex-ccp.tar.gz'):
+        pytest.skip('Data are downloaded.')
+    s = 'wget https://osf.io/hzq2x/download -O ex-ccp.tar.gz\n'
+    s += 'tar -xzf ex-ccp.tar.gz\n'
+    proc = Popen(s, shell=True)
+    proc.communicate()
+
+
+def test_sub01():
+    para = ccppara('ex-ccp/ccp.cfg')
+    para.rfpath = 'ex-ccp/RFresult'
+    para.stalist = 'ex-ccp/sta.lst'
+    para.stack_sta_list = ''
+    makedata(para)
+
+
+def test_sub02():
+    ccp = CCPProfile('ex-ccp/ccp.cfg')
+    # para.stalist = 'ex-ccp/sta.lst'
+    ccp.cpara.width = 40
+    ccp.initial_profile()
+    ccp.stack()
+    ccp.save_stack_data(format='dat')
+    os.remove(ccp.cpara.stack_sta_list)
+    os.remove(ccp.cpara.stackfile)
+
+def test_sub03():
+    ccp = CCPProfile('ex-ccp/ccp.cfg')
+    ccp.cpara.adaptive = True
+    ccp.cpara.stack_sta_list = 'ex-ccp/sta.lst'
+    ccp.initial_profile()
+    ccp.stack()
+    ccp.save_stack_data(format='dat')
+    os.remove(ccp.cpara.stackfile)
+
+if __name__ == '__main__':
+    test_sub03()
