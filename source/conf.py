4eae3fbdf9ed9fc6e791dc1e4bb4d38bbb6be3ea
xumi1993 <gomijianxu@gmail.com>
2022-02-14 17:18:53 +0800

    update ci

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
-import sphinx_rtd_theme
 import os
 import sys
 sys.path.insert(0, os.path.abspath('./seispy/seispy'))
